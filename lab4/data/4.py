bf02314fb4bdec7f36c888290cae5d3d3c22232f
Stas Li <ctac_ka6ah@mail.ru>
2024-02-15 15:08:25 +0600

    upgrade

--- lab4/data/4.py
+++ lab4/data/4.py
@@ -1,8 +1,16 @@
 from datetime import *
-from time import sleep
 
-x = datetime.now()
+def seconds(x, y):
+    x_date = datetime(year = x[0], month = x[1], day = x[2])
+    y_date = datetime(year = y[0], month = y[1], day = y[2])
 
-x_tomorrow = x + timedelta(seconds=50)
+    if x_date > y_date:
+        print((x_date-y_date).total_seconds())
+    else:
+        print((y_date-x_date).total_seconds())
 
-print((x_tomorrow - x).seconds)
+first_date = input("Enter first date time in format yyyy-mm-dd: ").split("-")
+second_date = input("Enter second date time in format yyyy-mm-dd: ").split("-")
+
+
+seconds(list(map(int, first_date)), list(map(int, second_date)))
